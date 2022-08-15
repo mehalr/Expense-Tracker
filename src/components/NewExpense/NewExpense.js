753d23ca300ab4e6a1462644be328e3ba32aa043
mehalr <mehalr612@gmail.com>
2022-08-15 15:08:35 +0530

    New expense component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,23 @@
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm'
+
+const NewExpense = (props) => {
+    const ExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+
+    }
+
+
+    return(
+        <div className='new-expense'>
+           <ExpenseForm onSaveExpenseData={ExpenseDataHandler}/>
+        </div>
+    );
+
+};
+
+export default NewExpense;
